f7b4d3814797a492e46f41f50496e4d5b8eaf8f0
Nikita Mittal <nikitamittal@Nikitas-MacBook-Air.local>
2018-08-01 14:46:39 +0530

    Task Comment  updated

--- TaskCommentForm.js
+++ TaskCommentForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-import InputElement from '../InputElement'
-import SubmitButton from '../SubmitButton'
+import InputElement from './InputElement'
+import SubmitButton from './SubmitButton'
 
 const TaskCommentForm = (props) => {
 
