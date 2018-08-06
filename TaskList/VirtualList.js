ad40204763ed1d54a7ebdab6a6b2343b0f494b4d
robins kumar gupta <robinskumargupta@robinss-MacBook-Air.local>
2018-08-06 16:39:36 +0530

    TaskList format modified.

--- TaskList/VirtualList.js
+++ TaskList/VirtualList.js
@@ -1,121 +1,112 @@
-/**
- * Created by Robins 
- * 4th Aug 2016
- */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import map from 'lodash/map';
 import { List, WindowScroller } from 'react-virtualized';
+// import {
+//     sortableContainer,
+//     sortableElement,
+//     arrayMove,
+//     DragLayer
+//   } from './react-sortable-multiple-hoc'
 
 //Custom Import
-import './VirtualList.css';
+import './TaskList.css';
 import TaskListHeading from './TaskListHeading';
 import TaskItem from './TaskItem'
 
 
+//const dragLayer = new DragLayer()
 
-
-const TaskList = (props) => {
+const TaskList = (props => {
 
     const {
-        taskList,
-        memberObj,
-        statusObj,
-        labelObj
+        allData,
+        sectionId,
     } = props;
 
-    const isSectionOpened = true;
+    console.log(allData);
 
-    function rowRenderer ({
-        index,       // Index of row
-        isScrolling, // The List is currently being scrolled
-        isVisible,   // This row is visible within the List (eg it is not an overscanned row)
-        key,         // Unique key within array of rendered rows
-        parent,      // Reference to the parent List (instance)
-        style        // Style object to be applied to row (to position it);
-                     // This must be passed through to the rendered row element.
-      }) {
-        const task = taskList[index];
+    const renderRow = (sectionId) => {
+        const tasks = allData.section.byId[sectionId].tasks;
+        const rowRenderer =  ({
+            index,       // Index of row
+            isScrolling, // The List is currently being scrolled
+            isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+            key,         // Unique key within array of rendered rows
+            parent,      // Reference to the parent List (instance)
+            style        // Style object to be applied to row (to position it);
+                         // This must be passed through to the rendered row element.
+          })  => {
+            
+            const task = tasks[index];
+            // Style is required since it specifies how the row is to be sized and positioned.
+            // React Virtualized depends on this sizing/positioning for proper scrolling behavior.
+            // By default, the List component provides following style properties:
+            //    position
+            //    left
+            //    top
+            //    height
+            //    width
+            // You can add additional class names or style properties as you would like.
+            // Key is also required by React to more efficiently manage the array of rows.
+            return (
+                <div style={style} key={key}>
+                    <TaskItem index={index} task={task} isActiveTaskSection  memberObj={allData.user.byId} statusObj={allData.status.byId} labelObj ={allData.label.byId}/>
+                </div>
+            )
+        }
 
-        // Style is required since it specifies how the row is to be sized and positioned.
-        // React Virtualized depends on this sizing/positioning for proper scrolling behavior.
-        // By default, the List component provides following style properties:
-        //    position
-        //    left
-        //    top
-        //    height
-        //    width
-        // You can add additional class names or style properties as you would like.
-        // Key is also required by React to more efficiently manage the array of rows.
-        return (
-            <div style={style} key={key}>
-                <TaskItem  task={task} isActiveTaskSection  memberObj={memberObj} statusObj={statusObj} labelObj ={labelObj}/>
-            </div>
-        )
+        return rowRenderer;
     }
     
 
+    const isSectionOpened = true;
+
+   
     return (
         <div>
-            <div style={{ background: "#fff", maxWidth: "800px", margin: "0 auto"}}>
-                <TaskListHeading heading="Active Tasks" type="fixed"/>
-                    {
-                        taskList && 
-                        taskList.length!==0 && 
-                        <div>        
-                            <WindowScroller>
-                                {({ height, isScrolling, registerChild, scrollTop }) => (
-                                    <div ref={registerChild}>
-                                        <List
-                                            width={800}
-                                            rowCount={taskList.length}
-                                            rowHeight={41}
-                                            rowRenderer={rowRenderer}
-                                            autoHeight
-                                            height={height}
-                                            isScrolling={isScrolling}
-                                            scrollTop={scrollTop}
-                                        />
-                                    </div>
-                                )}
-                            </WindowScroller>
+            {
+                map(allData.section.allIds, sectionId => {
+                    const section = allData.section.byId[sectionId];
+                    const taskRowRenderer = renderRow(sectionId);
+                    return (
+                        <div key={section.id} style={{ background: "#fff", maxWidth: "800px", margin: "0 auto"}}>
+                            <TaskListHeading id={section.id} heading={section.title} protected={section.isProtected} type="fixed"/>
+                            {
+                                section.tasks && 
+                                section.tasks.length!==0 && 
+                                <WindowScroller>
+                                    {({ height, isScrolling, registerChild, scrollTop }) => (
+                                        <div ref={registerChild}>
+                                            <List
+                                                width={800}
+                                                rowCount={section.tasks.length}
+                                                rowHeight={41}
+                                                rowRenderer={taskRowRenderer}
+                                                autoHeight
+                                                height={height}
+                                                isScrolling={isScrolling}
+                                                scrollTop={scrollTop}
+                                            />
+                                        </div>
+                                    )}
+                                </WindowScroller>
+                            }
                         </div>
-                    }
-            </div>
-            <div style={{ background: "#fff", maxWidth: "800px", margin: "0 auto"}}>
-                <TaskListHeading heading="All Tasks" type="fixed"/>
-                    {
-                        taskList && 
-                        taskList.length!==0 && 
-                        <div>        
-                            <WindowScroller>
-                                {({ height, isScrolling, registerChild, scrollTop }) => (
-                                    <div ref={registerChild}>
-                                        <List
-                                            width={800}
-                                            rowCount={taskList.length}
-                                            rowHeight={41}
-                                            rowRenderer={rowRenderer}
-                                            autoHeight
-                                            height={height}
-                                            isScrolling={isScrolling}
-                                            scrollTop={scrollTop}
-                                        />
-                                    </div>
-                                )}
-                            </WindowScroller>
-                        </div>
-                    }
-            </div>
+                    );
+                })
+            }
+            
         </div>
     )
-}
+});
 
 // Retrieve data from store as props
 function mapStateToProps(store) {
+    const taskListReducer = store.AllTaskReducer;
     return {
-
+        
     };
 }
 
