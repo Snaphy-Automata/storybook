240574c49716e9477f9c24d2c43abb1e64825db5
robins kumar gupta <robinskumargupta@robinss-MacBook-Air.local>
2018-08-06 14:35:59 +0530

    Working on Drag And Drop.

--- TaskList/TaskItem.js
+++ TaskList/TaskItem.js
@@ -4,11 +4,7 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
 import { Icon, Input, Popup  } from 'semantic-ui-react'
 import map from 'lodash/map';
-import {
-    SortableElement,
-    SortableHandle,
-  } from 'react-sortable-hoc';
-  
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 //Custom import..
 import './TaskList.css';
@@ -23,14 +19,14 @@ const COMPLETED_TASK_COLOR_CODE = "#1ed0c1";
 /**
  * Drag handle
  */
-const DragHandle = SortableHandle(() => (
-    <div className="task-list-item-drag-icon-container">
+const DragHandle = ({provided}) => (
+    <div  className="task-list-item-drag-icon-container">
         <Icon className="task-list-item-drag-icon" name="ellipsis vertical"></Icon>
         <Icon className="task-list-item-drag-icon" name="ellipsis vertical"></Icon>
     </div>
-)); // This can be any component you want
+); // This can be any component you want
 
-const TaskItem = SortableElement((props) => {
+const TaskItem = (props) => {
    
     const {
         task,
@@ -42,6 +38,7 @@ const TaskItem = SortableElement((props) => {
         onBlur,
         isNew,
         isActiveTaskSection,
+        index,
     } = props;
 
 
@@ -72,126 +69,130 @@ const TaskItem = SortableElement((props) => {
     //FIXME: When selected add `selected` class.
     const taskItemContainerClassName = `task-list-item-container`;
     return (
-        <div className="task-list-item-wrapper">
-            {!isNew &&
-                <div className="task-list-item-delayed-wrapper">
-                    <div className={taskItemContainerClassName} >
-                        <div className={delayedClassName}></div>
-                        <div className="task-list-item-side-bar-container">
-                            <div className={'task-list-item-side-line'}>
-                                <DragHandle />
+        <Draggable key={task.id} draggableId={task.id} index={index}>
+        {(provided, snapshot) => (
+            <div  className="task-list-item-wrapper">
+                {!isNew &&  
+                    <div  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="task-list-item-delayed-wrapper">
+                        <div  className={taskItemContainerClassName} >
+                            <div className={delayedClassName}></div>
+                            <div className="task-list-item-side-bar-container">
+                                <div className={'task-list-item-side-line'}>
+                                    <DragHandle provided={provided} />
+                                </div>
+                                <div className={'task-list-item-icon'}>
+                                    {iconObj.title && <TeamCircleIcon className="task-list-item-icon-team-circular" size="mini" src={iconObj.thumbnailUrl} title={iconObj.title} tooltip={iconObj.tooltip} />}
+                                    {iconObj.icon &&  <TeamCircleIcon className="task-list-item-icon-team-circular" size="mini" src={iconObj.thumbnailUrl} icon={iconObj.icon} tooltip={iconObj.tooltip} />}
+                                </div>
                             </div>
-                            <div className={'task-list-item-icon'}>
-                                {iconObj.title && <TeamCircleIcon className="task-list-item-icon-team-circular" size="mini" src={iconObj.thumbnailUrl} title={iconObj.title} tooltip={iconObj.tooltip} />}
-                                {iconObj.icon &&  <TeamCircleIcon className="task-list-item-icon-team-circular" size="mini" src={iconObj.thumbnailUrl} icon={iconObj.icon} tooltip={iconObj.tooltip} />}
+
+                            <div className="task-list-item-title">
+                                <div className="task-list-item-title-item">{taskHelper.getTitle()}</div>
                             </div>
-                        </div>
+                            <div className="task-list-item-other-container">
+                                <div className="task-list-item-status-duration-container">
+                                    {isActiveTaskSection && statusData &&
+                                        <div className="task-list-item-status" style={{ color: statusData.colorCode }}>{statusData.title}</div>
 
-                        <div className="task-list-item-title">
-                            <div className="task-list-item-title-item">{taskHelper.getTitle()}</div>
-                        </div>
-                        <div className="task-list-item-other-container">
-                            <div className="task-list-item-status-duration-container">
-                                {isActiveTaskSection && statusData &&
-                                    <div className="task-list-item-status" style={{ color: statusData.colorCode }}>{statusData.title}</div>
+                                    }
+                                    {!isActiveTaskSection && duration !== undefined &&
+                                        // Add duration class..
+                                        <div className="task-list-item-status">
+                                            <Icon name="clock outline" style={{ display: "inline", margin: '0' }}></Icon>
+                                            <span className="task-list-item-status-duration">{duration}</span>
+                                        </div>
+                                    }
+                                </div>
 
-                                }
-                                {!isActiveTaskSection && duration !== undefined &&
-                                    // Add duration class..
-                                    <div className="task-list-item-status">
-                                        <Icon name="clock outline" style={{ display: "inline", margin: '0' }}></Icon>
-                                        <span className="task-list-item-status-duration">{duration}</span>
+
+                                <div className="task-list-item-sub-task-attachment-container">
+                                    <div style={{ display: "inline-block", width: "60%" }}>
+                                        {
+                                            subTaskObj &&
+                                            <div>
+                                                <Icon name="unordered list" style={{ display: "inline", margin: '0' }}></Icon>
+                                                <div className="task-list-item-sub-task-stats">{subTaskObj.completed}/{subTaskObj.total}</div>
+                                            </div>
+                                        }
+
                                     </div>
-                                }
-                            </div>
 
+                                    <div style={{ display: "inline-block", width: "40%", textAlign: 'left' }}>
+                                        {
+                                            attachmentObj &&
+                                            <div>
+                                                <Icon name="attach" style={{ display: "inline" }}></Icon>
+                                                <div className="task-list-item-attachment-stats">{attachmentObj.total}</div>
+                                            </div>
+                                        }
 
-                            <div className="task-list-item-sub-task-attachment-container">
-                                <div style={{ display: "inline-block", width: "60%" }}>
-                                    {
-                                        subTaskObj &&
-                                        <div>
-                                            <Icon name="unordered list" style={{ display: "inline", margin: '0' }}></Icon>
-                                            <div className="task-list-item-sub-task-stats">{subTaskObj.completed}/{subTaskObj.total}</div>
-                                        </div>
-                                    }
+                                    </div>
+
 
                                 </div>
-
-                                <div style={{ display: "inline-block", width: "40%", textAlign: 'left' }}>
+                                <div className="task-list-item-tags-container">
                                     {
-                                        attachmentObj &&
-                                        <div>
-                                            <Icon name="attach" style={{ display: "inline" }}></Icon>
-                                            <div className="task-list-item-attachment-stats">{attachmentObj.total}</div>
+                                        labels &&
+                                        labels.length > 0 &&
+                                        <div className="task-list-item-tag-item">
+                                            <Label title={labels[0].title} color={labels[0].colorCode} tooltip={labels[0].title} style={{ float: 'left' }} />
+                                            {labels.length > 1 &&
+                                                <Label title="..." style={{ float: 'right' }} tooltip={labelObjData.tooltip} />}
                                         </div>
+
                                     }
-
                                 </div>
-
-
-                            </div>
-                            <div className="task-list-item-tags-container">
                                 {
-                                    labels &&
-                                    labels.length > 0 &&
-                                    <div className="task-list-item-tag-item">
-                                        <Label title={labels[0].title} color={labels[0].colorCode} tooltip={labels[0].title} style={{ float: 'left' }} />
-                                        {labels.length > 1 &&
-                                            <Label title="..." style={{ float: 'right' }} tooltip={labelObjData.tooltip} />}
+                                    !formattedDueDateObj.date &&
+                                    <div className="task-list-item-date-default-container">
+                                        <div style={{ position: "relative", top: "2px" }}>
+                                            <TeamCircleIcon className="task-list-item-icon-team-circular" icon="calendar alternate outline" size="tiny" tooltip="Assign Due Date"></TeamCircleIcon>
+                                        </div>
                                     </div>
-
                                 }
-                            </div>
-                            {
-                                !formattedDueDateObj.date &&
-                                <div className="task-list-item-date-default-container">
-                                    <div style={{ position: "relative", top: "2px" }}>
-                                        <TeamCircleIcon className="task-list-item-icon-team-circular" icon="calendar alternate outline" size="tiny" tooltip="Assign Due Date"></TeamCircleIcon>
-                                    </div>
+                                {
+                                    formattedDueDateObj.date &&
+                                    <Popup 
+                                        trigger={<div className="task-list-item-date-container" style={{ color: formattedDueDateObj.colorCode }}>
+                                            <Icon name="calendar minus outline" style={{ display: "inline" }}></Icon>
+                                            <div className="task-list-item-date-item" style={{ color: formattedDueDateObj.colorCode }}>{formattedDueDateObj.date}</div>
+                                        </div>}
+                                        content="Change Due Date"
+                                        position='bottom center'
+                                        inverted
+                                        style={{ fontSize: '10px', paddingRight: "20px", paddingLeft: "20px", maxWidth: "200px", letterSpacing: "0.5px", wordBreak: "break-word" }}
+                                        size='mini'>
+                                    </Popup>
+                                }
+                            </div> {/*Other Container div end*/}
+                        </div>
+                    </div>
+                }
+                {
+                    isNew &&
+                    <div className="task-list-item-delayed-wrapper">
+                        <div className={taskItemContainerClassName} >
+                            <div className={delayedClassName}></div>
+                            <div className="task-list-item-side-bar-container">
+                                <div className={'task-list-item-side-line'}>
                                 </div>
-                            }
-                            {
-                                formattedDueDateObj.date &&
-                                <Popup 
-                                    trigger={<div className="task-list-item-date-container" style={{ color: formattedDueDateObj.colorCode }}>
-                                        <Icon name="calendar minus outline" style={{ display: "inline" }}></Icon>
-                                        <div className="task-list-item-date-item" style={{ color: formattedDueDateObj.colorCode }}>{formattedDueDateObj.date}</div>
-                                    </div>}
-                                    content="Change Due Date"
-                                    position='bottom center'
-                                    inverted
-                                    style={{ fontSize: '10px', paddingRight: "20px", paddingLeft: "20px", maxWidth: "200px", letterSpacing: "0.5px", wordBreak: "break-word" }}
-                                    size='mini'>
-                                </Popup>
-                            }
-                        </div> {/*Other Container div end*/}
-                    </div>
-                </div>
-            }
-            {
-                isNew &&
-                <div className="task-list-item-delayed-wrapper">
-                    <div className={taskItemContainerClassName} >
-                        <div className={delayedClassName}></div>
-                        <div className="task-list-item-side-bar-container">
-                            <div className={'task-list-item-side-line'}>
+                                <div className={'task-list-item-icon'}>
+                                </div>
                             </div>
-                            <div className={'task-list-item-icon'}>
-                            </div>
-                        </div>
 
-                        <div className="task-list-item-new-task-title">
-                            <div className="task-list-item-new-task-container">
-                                <Field name="title" placeholder="Write Task" transparent autoFocus fluid className="task-list-item-new-task" component={InputField}/>
+                            <div className="task-list-item-new-task-title">
+                                <div className="task-list-item-new-task-container">
+                                    <Field name="title" placeholder="Write Task" transparent autoFocus fluid className="task-list-item-new-task" component={InputField}/>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            }
-        </div>
+                }
+            </div>
+         )}
+        </Draggable>
     );
-});
+};
 
 const TaskItemForm = reduxForm({
     form: "taskForm",
--- TaskList/TaskList.js
+++ TaskList/TaskList.js
@@ -2,17 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import map from 'lodash/map';
-import {
-    SortableContainer,
-  } from 'react-sortable-hoc';
-
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { List, WindowScroller } from 'react-virtualized';
 import './TaskList.css';
 
 import TaskListHeading from './TaskListHeading';
 import TaskItem from './TaskItem'
 
-
-const TaskList = SortableContainer((props) => {
+const TaskList = (props) => {
 
     const {
         sectionId,
@@ -23,28 +20,88 @@ const TaskList = SortableContainer((props) => {
         labelObj
     } = props;
 
+    function rowRenderer ({
+        index,       // Index of row
+        isScrolling, // The List is currently being scrolled
+        isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+        key,         // Unique key within array of rendered rows
+        parent,      // Reference to the parent List (instance)
+        style        // Style object to be applied to row (to position it);
+                     // This must be passed through to the rendered row element.
+      }) {
+        const task = taskList[index];
+
+        // Style is required since it specifies how the row is to be sized and positioned.
+        // React Virtualized depends on this sizing/positioning for proper scrolling behavior.
+        // By default, the List component provides following style properties:
+        //    position
+        //    left
+        //    top
+        //    height
+        //    width
+        // You can add additional class names or style properties as you would like.
+        // Key is also required by React to more efficiently manage the array of rows.
+        return (
+            <div style={style} key={key}>
+                <TaskItem index={index} task={task} isActiveTaskSection  memberObj={memberObj} statusObj={statusObj} labelObj ={labelObj}/>
+            </div>
+        )
+    }
+
     const isSectionOpened = true;
+
+    const reorder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+      
+        return result;
+      };
+
+    const onDragEnd = (result) => {
+        console.log("On Drag End", result);
+    };
 
     return (
         <div>
             <div style={{ background: "#fff", maxWidth: "800px", margin: "0 auto"}}>
                 <TaskListHeading heading="Active Tasks" type="fixed"/>
-                {taskList && taskList.length!==0 && <div>
-                    {
-                        map(taskList, function(task, index){
-
-                            return(
-                                <TaskItem index={index} key={index} task={task} isActiveTaskSection  memberObj={memberObj} statusObj={statusObj} labelObj ={labelObj}/>
-                            )
-                        })
-                    }
-                </div>}
-
+                {
+                    taskList && 
+                    taskList.length!==0 && 
+                    <WindowScroller>
+                        {({ height, isScrolling, registerChild, scrollTop }) => (
+                            <div ref={registerChild}>
+                                <DragDropContext onDragEnd={onDragEnd}>
+                                    <Droppable droppableId="droppable">
+                                    {(provided, snapshot) => (
+                                        <div ref={provided.innerRef} >
+                                            {
+                                                <List
+                                                    width={800}
+                                                    rowCount={taskList.length}
+                                                    rowHeight={41}
+                                                    rowRenderer={rowRenderer}
+                                                    autoHeight
+                                                    height={height}
+                                                    isScrolling={isScrolling}
+                                                    scrollTop={scrollTop}
+                                                />
+                                            }
+                                        </div>
+                                    )}
+                                </Droppable>
+                            </DragDropContext>
+                                
+                            </div>
+                        )}
+                    </WindowScroller>
+                }
             </div>
         </div>
     )
 
-});
+};
 
 // Retrieve data from store as props
 function mapStateToProps(store) {
